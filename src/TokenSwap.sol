3a691aac65c60614253b0f1409c4d1bcc95aaf85
William Phan <williamphan@MacBook-Pro-2.fios-router.home>
2022-09-25 17:19:38 -0400

    added token swap

---
+++ src/TokenSwap.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+
+// import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract TokenSwap {
+    IERC20 public token1;
+    address public owner1;
+    uint256 public amount1;
+
+    IERC20 public token2;
+    address public owner2;
+    uint256 public amount2;
+
+    // allow creator to determin wwwwwwdaswayyfujyfjyfjyhfjyhfjfjhgfjh
+    constructor(
+        address _token1,
+        address _owner1,
+        uint256 _amount1,
+        address _token2,
+        address _owner2,
+        uint256 _amount2
+    ) {
+        token1 = IERC20(_token1);
+        owner1 = _owner1;
+        amount1 = _amount1;
+        token2 = IERC20(_token2);
+        owner2 = _owner2;
+        amount2 = _amount2;
+    }
+
+    function Swap() public {
+        // require one of owners to initiate
+        require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
+        require(
+            token1.allowance(owner1, address(this)) >= amount1,
+            "Token 1 allowance too low"
+        );
+        require(
+            token2.allowance(owner2, address(this)) >= amount2,
+            "Token 2 allowance too low"
+        );
+
+        _safeTransferFrom(token1, owner1, owner2, amount1);
+        _safeTransferFrom(token2, owner2, owner1, amount2);
+    }
+
+    function _safeTransferFrom(
+        IERC20 token,
+        address sender,
+        address recipient,
+        uint256 amount
+    ) private {
+        bool sent = token.transferFrom(sender, recipient, amount);
+        require(sent, "Token transfer failed");
+    }
+}
